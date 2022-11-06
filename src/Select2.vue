07f4850cf9241502c2492cdd05a09a92edfc9e61
Jacky supit <jsupit@gmail.com>
2022-11-06 21:22:05 +0700

    typo :(

--- src/Select2.vue
+++ src/Select2.vue
@@ -179,7 +179,7 @@ export default {
         if(xJquery == 3) {
           let jqueryScript = document.createElement('script');
           jqueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js');
-          document.head.appendChild(my_awesome_script);
+          document.head.appendChild(jqueryScript);
         }
 
         console.log("starting the select2 - waiting xJquery: ", xJquery);
@@ -261,7 +261,7 @@ export default {
           if(xx == 3) {
             let select2Script = document.createElement('script');
             select2Script.setAttribute('src','https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js');
-            document.head.appendChild(my_awesome_script);
+            document.head.appendChild(select2Script);
           }
 
 
